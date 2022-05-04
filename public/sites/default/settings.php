848193d0e3a393b57b646dbd3c0a03748117ada9
Jussiles <juss.leskinen@gmail.com>
2022-05-04 16:00:58 +0300

    Add AZURE_SQL_SSL_CA_PATH back

--- public/sites/default/settings.php
+++ public/sites/default/settings.php
@@ -29,6 +29,20 @@ $databases['default']['default'] = [
 
 $settings['hash_salt'] = getenv('DRUPAL_HASH_SALT') ?: '000';
 
+if ($ssl_ca_path = getenv('AZURE_SQL_SSL_CA_PATH')) {
+  $databases['default']['default']['pdo'] = [
+    \PDO::MYSQL_ATTR_SSL_CA => $ssl_ca_path,
+    \PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => FALSE,
+  ];
+  // Azure specific filesystem fixes.
+  $settings['php_storage']['twig']['directory'] = '/tmp';
+  $settings['php_storage']['twig']['secret'] = $settings['hash_salt'];
+  $settings['file_chmod_directory'] = 16895;
+  $settings['file_chmod_file'] = 16895;
+
+  $config['system.performance']['cache']['page']['max_age'] = 86400;
+}
+
 // Only in Wodby environment.
 // @see https://wodby.com/docs/stacks/drupal/#overriding-settings-from-wodbysettingsphp
 if (isset($_SERVER['WODBY_APP_NAME'])) {
