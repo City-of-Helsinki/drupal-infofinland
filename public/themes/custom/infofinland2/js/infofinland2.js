24184479009a6a54f08007525574d500a329d18a
Jussi Jokinen <jussi.jokinen@citrus.fi>
2023-10-26 14:22:47 +0300

    IFU-911: Created new sub theme with Drush

---
+++ public/themes/custom/infofinland2/js/infofinland2.js
@@ -0,0 +1,21 @@
+/**
+ * @file
+ * infofinland2 behaviors.
+ */
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  /**
+   * Behavior description.
+   */
+  Drupal.behaviors.infofinland2 = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (jQuery, Drupal));
