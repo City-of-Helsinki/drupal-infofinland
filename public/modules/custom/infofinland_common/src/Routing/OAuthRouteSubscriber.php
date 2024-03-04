4e979e16f67ff8f5b96ebff50c1ce0be9d2b7cc1
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2024-03-04 08:29:00 +0200

    UHF-9624: Accept any credentials in local environment

---
+++ public/modules/custom/infofinland_common/src/Routing/OAuthRouteSubscriber.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\infofinland_common\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Drupal\infofinland_common\Controller\OAuth2TokenController;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Intercept token auth in local environment.
+ */
+class OAuthRouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alterRoutes(RouteCollection $collection): void {
+    if ($route = $collection->get('oauth2_token.token')) {
+      $route->setDefaults(array(
+        '_controller' => OAuth2TokenController::class . '::token',
+      ));
+    }
+  }
+
+}
+
